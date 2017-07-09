3f2259abff68d4569959396fd29cd7fbe9d7d9bc
middleshin <middleshin@middleshin-PC>
2017-07-09 14:12:08 +0900

    Signed-off-by: middleshin <middleshin@middleshin-PC>

--- src/myApp/server/file/FileUpload.java
+++ src/myApp/server/file/FileUpload.java
@@ -18,6 +18,12 @@ import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
 import org.apache.ibatis.session.SqlSession;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import myApp.client.sys.model.FileModel;
@@ -123,42 +129,68 @@ public class FileUpload implements javax.servlet.Servlet {
 			ServletFileUpload serveltFileUpload = new ServletFileUpload(factory);
 			FileItem fileItem = serveltFileUpload.parseRequest(request).get(0); // 처음 하나의 파일만 가져온다.  
 
-			// 아직 파일로 저장하기 전이다. 
+			// 파일로 저장하지 않고 stream으로 파일을 읽는다. 저장할데가 없잖아.. 
 			InputStream inputStream = fileItem.getInputStream(); 
+			
+			@SuppressWarnings("resource")
 			XSSFWorkbook workbook = new XSSFWorkbook(inputStream);    
-			
-			// http://javaslave.tistory.com/78 에서 참조할 것. 
-			
-			String fileId = request.getParameter("fileId");
 
-			FileModel fileModel = new FileModel();
-			String fileName = fileItem.getName();	// file upload시에만 사용된다.
+			//http://javaslave.tistory.com/78 에서 참조할 것. 
 			
-			if(fileId == null || "null".equals(fileId)){
+			XSSFSheet curSheet; 
+			XSSFRow curRow; 
+			// XSSFCell curCell; 
+			
+			for(int sheetIndex = 0; sheetIndex < workbook.getNumberOfSheets(); sheetIndex++){
 				
-				// get file id 
-				IsNewData isNewData = new IsNewData(); 
+				curSheet = workbook.getSheetAt(sheetIndex);
 				
-				Double size = Double.parseDouble((fileItem.getSize()/1024) + ""); 
-				fileModel.setSize(size); 
+				for(int rowIndex=0; rowIndex<curSheet.getPhysicalNumberOfRows(); rowIndex++){
+					
+					if(rowIndex != 0){
+						
+						curRow = curSheet.getRow(rowIndex);
+						
+						String var = curRow.getCell(0).getStringCellValue(); 
+						
+						if(var != null){
+							System.out.println("sheet data is " + var); 
+						}
+					}
+				}
 				
-				fileModel.setDelDate(null); 
-				fileModel.setNote(null);
-				
-		 
 			}
-			 
-			File subDir  = new File(this.getUploadPath(fileId));
-	        if(!subDir.exists()) {
-	        	subDir.mkdir(); 
-	        }
-
-	        File file = new File(this.getUploadPath(fileId), fileId);
-    	    file.deleteOnExit(); // 있으면 먼저 지워라.
-            fileItem.write(file);
-            
-//            sqlSession.commit();
-//            sqlSession.close();
+//			
+//			String fileId = request.getParameter("fileId");
+//
+//			FileModel fileModel = new FileModel();
+//			String fileName = fileItem.getName();	// file upload시에만 사용된다.
+//			
+//			if(fileId == null || "null".equals(fileId)){
+//				
+//				// get file id 
+//				IsNewData isNewData = new IsNewData(); 
+//				
+//				Double size = Double.parseDouble((fileItem.getSize()/1024) + ""); 
+//				fileModel.setSize(size); 
+//				
+//				fileModel.setDelDate(null); 
+//				fileModel.setNote(null);
+//				
+//		 
+//			}
+//			 
+//			File subDir  = new File(this.getUploadPath(fileId));
+//	        if(!subDir.exists()) {
+//	        	subDir.mkdir(); 
+//	        }
+//
+//	        File file = new File(this.getUploadPath(fileId), fileId);
+//    	    file.deleteOnExit(); // 있으면 먼저 지워라.
+//            fileItem.write(file);
+//            
+////            sqlSession.commit();
+////            sqlSession.close();
             
             setResult(response, "파일을 성공적으로 등록하였습니다.");
             
